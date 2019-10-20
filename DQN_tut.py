e745f76f0af84f69dcc79e3d1169c5badd58cf68
temoTxt <morris.trey.j@gmail.com>
2019-10-20 17:33:14 -0400

    try to use matplotlib backend

--- DQN_tut.py
+++ DQN_tut.py
@@ -3,6 +3,7 @@ import math
 import random
 import numpy as np
 import matplotlib
+matplotlib.use('GTK')
 import matplotlib.pyplot as plt
 from collections import namedtuple
 from itertools import count
